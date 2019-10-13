eaefa82a5b8a94413cf5b17a6bc9b80116d9210b
Nathan Rawling <nathan.rawling@gmail.com>
2019-10-13 16:30:00 -0400

    Working prototype

--- nrawling/BOFH_Excuse/app.py
+++ nrawling/BOFH_Excuse/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask import render_template
+from excuse import *
+
 app = Flask(__name__)
+
+excuses = load_excuses()
 
 @app.route('/')
 def index():
-    return '<h1>Hello world!</h1>'
+    return render_template('index.html', excuse=random_excuse(excuses))
 
 if __name__ == '__main__':
     app.run(debug=True)
